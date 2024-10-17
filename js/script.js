7129e2cb5fc18876d6805c47126beda37d94d150
Messias Machado <messiasmachado1976@gmail.com>
2024-10-17 15:42:22 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+/*==== Transformar o manu hamburguer em X ====*/
+
+const menuhamburguer = document.querySelector('.menu-hamburguer')
+menuhamburguer.addEventListener('click', () =>{
+    toggleMenu();
+
+});
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuhamburguer.classList.toggle('change');
+
+    if (
+        menuhamburguer.classList.contains('change')
+    ) {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+    
+}
